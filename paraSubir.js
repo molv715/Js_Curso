6635110e5c360b851cb41adf351f82e9ff464a2b
molv715 <molv715@gmail.com>
2021-08-31 17:50:02 -0400

    [FIX] cambios

--- paraSubir.js
+++ paraSubir.js
@@ -1,1 +1,14 @@
-dfgdfg
+
+[ADD] erroneo comit
+
+[ADD] adicione archivos
+[FIX]cambie datos
+git clone httpsetc clonas a tu pc el proyecto
+
+git push pushea tu trabajo
+git pull actualizas el proyecto 
+para mandar tu trabajos siempre un comit y luego el pusheo
+
+git checkout <nombre-de-la-rama>
+
+git push origin manny

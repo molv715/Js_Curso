ed1522b536cbd9b214ecfae9befeb09d08e85136
molv715 <molv715@gmail.com>
2021-08-31 17:53:14 -0400

    [FIX] cambios

--- paraSubir.js
+++ paraSubir.js
@@ -12,3 +12,4 @@ para mandar tu trabajos siempre un comit y luego el pusheo
 git checkout <nombre-de-la-rama>
 
 git push origin manny
+//https://www.freecodecamp.org/espanol/news/10-comandos-de-git-que-todo-desarrollador-deberia-saber/
